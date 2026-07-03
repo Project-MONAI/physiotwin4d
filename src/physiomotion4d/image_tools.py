747a508a7b5f018832c37c7d7e1b80b34ac9e240
Stephen Aylward <saylward@nvidia.com>
2026-07-03 12:07:50 -0400

    ENH: Resolve itk image type wrapping limitations

--- src/physiomotion4d/image_tools.py
+++ src/physiomotion4d/image_tools.py
@@ -364,22 +364,40 @@ class ImageTools(PhysioMotion4DBase):
             (background is 0).
         """
         ImageType = type(image)
+        dimension = image.GetImageDimension()
+        # RelabelComponentImageFilter is only wrapped for an itk.ULL input
+        # paired with an itk.UC/itk.US/itk.SS output (not itk.ULL -> itk.ULL,
+        # and not the itk.UL that the functional
+        # connected_component_image_filter defaults to), so request the
+        # component and relabeled types explicitly.
+        ComponentImageType = itk.Image[itk.ULL, dimension]
+        RelabeledImageType = itk.Image[itk.US, dimension]
 
-        connected_component_image = itk.connected_component_image_filter(
-            image, fully_connected=fully_connected
-        )
-        relabeled_image = itk.relabel_component_image_filter(
-            connected_component_image, sort_by_object_size=True
-        )
-        result = itk.binary_threshold_image_filter(
-            Input=relabeled_image,
-            LowerThreshold=1,
-            UpperThreshold=1,
-            InsideValue=foreground_value,
-            OutsideValue=0,
-        )
-        if type(result) is not ImageType:
-            result = itk.cast_image_filter(result, ttype=(type(result), ImageType))
+        cc_filter = itk.ConnectedComponentImageFilter[
+            ImageType, ComponentImageType
+        ].New()
+        cc_filter.SetInput(image)
+        cc_filter.SetFullyConnected(fully_connected)
+        cc_filter.Update()
+
+        relabel_filter = itk.RelabelComponentImageFilter[
+            ComponentImageType, RelabeledImageType
+        ].New()
+        relabel_filter.SetInput(cc_filter.GetOutput())
+        relabel_filter.SetSortByObjectSize(True)
+        relabel_filter.Update()
+
+        threshold_filter = itk.BinaryThresholdImageFilter[
+            RelabeledImageType, ImageType
+        ].New()
+        threshold_filter.SetInput(relabel_filter.GetOutput())
+        threshold_filter.SetLowerThreshold(1)
+        threshold_filter.SetUpperThreshold(1)
+        threshold_filter.SetInsideValue(foreground_value)
+        threshold_filter.SetOutsideValue(0)
+        threshold_filter.Update()
+        result = threshold_filter.GetOutput()
+        result.DisconnectPipeline()
         return result
 
     @overload
