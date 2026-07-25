c4077ae11672b37e3780eb946a081f71380d278e
Stephen Aylward <saylward@nvidia.com>
2026-07-25 11:18:21 -0400

    ENH: Release 2026.07.3

--- src/physiotwin4d/__init__.py
+++ src/physiotwin4d/__init__.py
@@ -20,7 +20,7 @@ Main Components:
     - PhysioTwin4DBase: Base class with standardized logging and debug settings
 """
 
-__version__ = "2026.07.2"
+__version__ = "2026.07.3"
 
 import importlib.util as _importlib_util
 import warnings as _warnings
